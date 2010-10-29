579b0427620d6a948f225e6b3bca75a38140ac48
digdog <digdog@mac.com>
2010-10-29 01:06:16 -0700

    Workaround version, search //FIXME: for detail

---
+++ DDAlertPrompt.h
@@ -0,0 +1,24 @@
+//
+//  DDAlertPrompt.h
+//  DDAlertPrompt
+//
+//  Created by digdog on 10/27/10.
+//  Copyright 2010 Ching-Lan 'digdog' HUANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DDAlertPrompt : UIAlertView <UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate> {
+	@private
+	UITableView *tableView_;
+	UITextField *plainTextField_;
+	UITextField *secretTextField_;
+}
+
+@property(nonatomic, retain, readonly) UITextField *plainTextField;
+@property(nonatomic, retain, readonly) UITextField *secretTextField;
+
+- (id)initWithTitle:(NSString *)title delegate:(id /*<UIAlertViewDelegate>*/)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitle:(NSString *)otherButtonTitles;
+
+@end
---
+++ DDAlertPrompt.m
@@ -0,0 +1,147 @@
+//
+//  DDAlertPrompt.m
+//  DDAlertPrompt
+//
+//  Created by digdog on 10/27/10.
+//  Copyright 2010 Ching-Lan 'digdog' HUANG. All rights reserved.
+//
+
+#import "DDAlertPrompt.h"
+#import <QuartzCore/QuartzCore.h>
+
+@interface DDAlertPrompt () 
+@property(nonatomic, retain) UITableView *tableView;
+@property(nonatomic, retain) UITextField *plainTextField;
+@property(nonatomic, retain) UITextField *secretTextField;
+- (void)orientationDidChange:(NSNotification *)notification;
+@end
+
+
+@implementation DDAlertPrompt
+
+@synthesize tableView = tableView_;
+@synthesize plainTextField = plainTextField_;
+@synthesize secretTextField = secretTextField_;
+
+- (id)initWithTitle:(NSString *)title delegate:(id /*<UIAlertViewDelegate>*/)delegate cancelButtonTitle:(NSString *)cancelButtonTitle otherButtonTitle:(NSString *)otherButtonTitles {
+
+	if ((self = [super initWithTitle:title message:@"\n\n\n" delegate:delegate cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles, nil])) {
+		// If you uncomment below, UITextFields in tableview will show characters when typing.
+		[self addSubview:self.plainTextField];
+
+		tableView_ = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+		tableView_.delegate = self;
+		tableView_.dataSource = self;		
+		tableView_.scrollEnabled = NO;
+		tableView_.opaque = NO;
+		tableView_.layer.cornerRadius = 3.0f;
+		tableView_.editing = YES;
+		[self addSubview:tableView_];
+								
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(orientationDidChange:) name:UIDeviceOrientationDidChangeNotification object:nil];        
+		[[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
+	}
+	return self;
+}
+
+- (void)dealloc {
+	[[NSNotificationCenter defaultCenter] removeObserver:self name:UIDeviceOrientationDidChangeNotification object:nil];
+	[[UIDevice currentDevice] endGeneratingDeviceOrientationNotifications];
+	
+	[tableView_ setDataSource:nil];
+	[tableView_ setDelegate:nil];
+	[tableView_ release];
+    [super dealloc];
+}
+
+#pragma mark layout
+
+- (void)layoutSubviews {
+	self.tableView.rowHeight = 28.0f;
+	self.tableView.frame = CGRectMake(12.0f, 51.0f, 260.0f, 56.0f);		
+
+	// We assume keyboard is on.
+	if ([[UIDevice currentDevice] isGeneratingDeviceOrientationNotifications]) {
+		if (UIDeviceOrientationIsPortrait([UIDevice currentDevice].orientation)) {
+			self.center = CGPointMake(160.0f, (480.0f - 216.0f)/2);
+		} else {
+			self.center = CGPointMake(240.0f, (320.0f - 162.0f)/2 - 20.0f);
+		}
+	}
+}
+
+- (void)orientationDidChange:(NSNotification *)notification {
+	[self setNeedsLayout];
+}
+
+#pragma mark Accessors
+
+- (UITextField *)plainTextField {
+
+	if (!plainTextField_) {
+		plainTextField_ = [[UITextField alloc] initWithFrame:CGRectMake(5.0f, 0.0f, 255.0f, 28.0f)];
+		plainTextField_.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+		plainTextField_.delegate = self;
+		plainTextField_.clearButtonMode = UITextFieldViewModeWhileEditing;
+		plainTextField_.placeholder = @"Nickname or Email";
+	}
+	return plainTextField_;
+}
+
+- (UITextField *)secretTextField {
+	
+	if (!secretTextField_) {
+		secretTextField_ = [[UITextField alloc] initWithFrame:CGRectMake(5.0f, 0.0f, 255.0f, 28.0f)];
+		secretTextField_.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+		secretTextField_.delegate = self;
+		secretTextField_.secureTextEntry = YES;
+		secretTextField_.clearButtonMode = UITextFieldViewModeWhileEditing;
+		secretTextField_.placeholder = @"Password";
+	}
+	return secretTextField_;
+}
+
+#pragma mark UITextFieldDelegate
+
+- (BOOL)textFieldShouldBeginEditing:(UITextField *)textField {
+    return YES;
+}
+
+#pragma mark UITableViewDataSource & UITableViewDelegate
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+	return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	return 2;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+	static NSString *AlertPromptCellIdentifier = @"DDAlertPromptCell";
+
+    UITableViewCell *cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:AlertPromptCellIdentifier];
+    if (cell == nil) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:AlertPromptCellIdentifier] autorelease];
+    }
+	
+	if (![cell.contentView.subviews count]) {
+		if (indexPath.row) {
+			[cell.contentView addSubview:self.secretTextField];			
+		} else {
+			[cell.contentView addSubview:self.plainTextField];
+		}		
+	}
+    return cell;	
+}
+
+- (BOOL)tableView:(UITableView *)tableView shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewCellEditingStyleNone;
+}
+
+@end
